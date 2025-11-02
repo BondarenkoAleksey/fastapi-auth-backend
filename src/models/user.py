868ef6f1fafd938024a01ae75313aaa7e894f7f9
user <bonddd@gmail.com>
2025-11-02 23:33:17 +0300

    feat: add user model and async database session setup

---
+++ src/models/user.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(50), nullable=False)
+    lastname = Column(String(50), nullable=False)
+    email = Column(String(100), unique=True, index=True, nullable=False)
+    hashed_password = Column(String(100), nullable=False)
+    is_active = Column(Boolean, default=True, nullable=False)
