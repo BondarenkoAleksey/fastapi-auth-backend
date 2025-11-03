45b21f532d168d6d1d9fee11e39253b16e15416b
user <bonddd@gmail.com>
2025-11-03 17:53:44 +0300

    feat: implement user authentication and management

--- src/models/user.py
+++ src/models/user.py
@@ -1,7 +1,13 @@
-from sqlalchemy import Column, Integer, String, Boolean
+import enum
+
+from sqlalchemy import Column, Integer, String, Boolean, Enum
 from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
+
+class UserRole(enum.Enum):
+    user = "user"
+    admin = "admin"
 
 class User(Base):
     __tablename__ = "users"
@@ -11,4 +17,5 @@ class User(Base):
     lastname = Column(String(50), nullable=False)
     email = Column(String(100), unique=True, index=True, nullable=False)
     hashed_password = Column(String(100), nullable=False)
+    role = Column(Enum(UserRole), default=UserRole.user, nullable=False)
     is_active = Column(Boolean, default=True, nullable=False)
